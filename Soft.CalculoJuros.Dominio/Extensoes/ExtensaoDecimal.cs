1a3222cfb8d2d322bbd59d9e3a4e00aee751bfcc
rafaelbitencourt <rafael_silbit@hotmail.com>
2021-08-19 19:01:24 -0300

    Criado domínio com testes

---
+++ Soft.CalculoJuros.Dominio/Extensoes/ExtensaoDecimal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Soft.CalculoJuros.Dominio.Extensoes
+{
+    public static class ExtensaoDecimal
+    {
+        public static decimal Potencia(this decimal valor, int potencia)
+        {
+            return (decimal)Math.Pow((double)valor, potencia);
+        }
+
+        public static decimal Truncar(this decimal valor, int precisao)
+        {
+            decimal num = (decimal)Math.Pow(10.0, precisao);
+            return Math.Truncate(num * valor) / num;
+        }
+    }
+}
