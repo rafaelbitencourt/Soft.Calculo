0dd918e27a19afe54441839203cbf0f7ba9b5039
rafaelbitencourt <rafael_silbit@hotmail.com>
2021-08-19 19:34:45 -0300

    Criado a API

---
+++ Soft.CalculoJuros.API/Controllers/CalculoJurosController.cs
@@ -0,0 +1,23 @@
+﻿using Soft.CalculoJuros.Aplicacao;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Soft.CalculoJuros.API.Controllers
+{
+    [ApiController]
+    [Route("calculajuros")]
+    public class CalculoJurosController : ControllerBase
+    {
+        private readonly IAplicCalculo _aplicCalculo;
+
+        public CalculoJurosController(IAplicCalculo aplicCalculo)
+        {
+            _aplicCalculo = aplicCalculo;
+        }
+
+        [HttpGet]
+        public string CalcularJuros([FromQuery] decimal valorinicial, [FromQuery] int meses)
+        {
+            return _aplicCalculo.CalcularJuros(valorinicial, meses).ToString("0.00");
+        }
+    }
+}
